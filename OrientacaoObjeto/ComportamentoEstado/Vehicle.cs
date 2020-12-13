84ad47da765063b25b5a4d8d2a616cad1215f736
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2020-12-12 22:28:04 -0300

    Iniciando estrutura de estudo de OOP

---
+++ OrientacaoObjeto/ComportamentoEstado/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OrientacaoObjeto.ComportamentoEstado
+{
+    public class Vehicle
+    {
+        public string VehicleModel { get; set; }
+        public DateTime ManufacturYear { get; set; }
+
+        public int CalulateYearUse()
+        {
+            var dateNow = DateTime.Now;
+            var dateUse = dateNow.Year - ManufacturYear.Year;
+
+            if (dateNow < ManufacturYear.AddYears(dateUse)) dateUse--;
+
+            return dateUse;
+        }
+    }
+}
---
+++ OrientacaoObjeto/Heranca/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OrientacaoObjeto.ComportamentoEstado;
+
+namespace OrientacaoObjeto.Heranca
+{
+    public class Car : Vehicle
+    {
+        public DateTime ModelYear { get; set; }
+        public string Register { get; set; }
+
+        public class Process
+        {
+            public void Run()
+            {
+                var car = new Car()
+                {
+                    VehicleModel = "Fusca",
+                    ManufacturYear = Convert.ToDateTime("1968/01/01"),
+                    ModelYear = Convert.ToDateTime("1969/01/01"),
+                    Register = "12346578"
+                };
+
+                car.CalulateYearUse();
+            }
+        }
+    }
+}
