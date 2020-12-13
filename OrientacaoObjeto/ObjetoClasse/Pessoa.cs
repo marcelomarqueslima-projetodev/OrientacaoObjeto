53a14e67f1dfa14b9841511de657d7592470dbaf
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2020-12-12 21:56:41 -0300

    Adicionar arquivos de projeto.

---
+++ OrientacaoObjeto/ObjetoClasse/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace OrientacaoObjeto.ObjetoClasse
+{
+    public class Pessoa
+    {
+        public class Objeto
+        {
+            public Objeto()
+            {
+                //Definição de Objeto
+                var pessoa = new Pessoa
+                {
+                    Name = "João",
+                    Address = "Rua Japão, 25",
+                    Phone = "+55 (41) 99999-9999",
+                    Age = 38
+                };
+            }
+        }
+
+        //Definição de Classe
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
